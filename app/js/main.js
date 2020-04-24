8af198c47b0327e8e74d7d9cd75a024fe3ee5f93
FEN <fentisov86@gmail.com>
2020-04-24 18:42:46 +0300

    added body lock

--- app/js/main.js
+++ app/js/main.js
@@ -1,69 +1,86 @@
 (function ($, undefined) {
-
-  $('.burger-menu').on('click', function () {
-    $('.main-nav').addClass('active');
+  $(".burger-menu").on("click", function () {
+    $(".main-nav").addClass("active");
+    $("body").addClass("lock");
   });
-  $('.close-btn').on('click', function () {
-    $('.main-nav').removeClass('active');
+  $(".close-btn").on("click", function () {
+    $(".main-nav").removeClass("active");
+    $("body").removeClass("lock");
   });
 
-  $('.slider-cards').slick({
-    mobileFirst: true,
-    arrows: false,
-    slidesToShow: 1.2,
-    lazyLoad: 'ondemand',
-    infinite: false,
-    adaptiveHeight: true,
-    dots: true,
-    responsive: [
-      {
-        breakpoint: 901,
-        settings: "unslick"
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2.5
+  $(window).on("load resize orientationchange", function () {
+    $(".slider-cards").each(function () {
+      var $slider = $(this);
+      if ($(window).width() > 901) {
+        if ($slider.hasClass("slick-initialized")) {
+          $slider.slick("unslick");
         }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2
+      } else {
+        if (!$slider.hasClass("slick-initialized")) {
+          $slider.slick({
+            mobileFirst: true,
+            arrows: false,
+            slidesToShow: 1.2,
+            lazyLoad: "ondemand",
+            infinite: false,
+            adaptiveHeight: true,
+            dots: true,
+            responsive: [
+              {
+                breakpoint: 900,
+                settings: {
+                  slidesToShow: 2.5,
+                  dots: false,
+                },
+              },
+              {
+                breakpoint: 767,
+                settings: {
+                  slidesToShow: 2.5,
+                },
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 2,
+                },
+              },
+            ],
+          });
         }
-      },
-    ]
+      }
+    });
   });
 
-  var $coachesSlider = $('.coaches-slider').slick({
+  var $coachesSlider = $(".coaches-slider").slick({
     arrows: true,
     slidesToShow: 2,
     slidesToScroll: 2,
-    lazyLoad: 'ondemand',
+    lazyLoad: "ondemand",
     infinite: false,
     responsive: [
       {
         breakpoint: 1200,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2
-        }
+          slidesToScroll: 2,
+        },
       },
       {
         breakpoint: 900,
         settings: {
           slidesToShow: 2.5,
           dots: true,
-          arrows: false
-        }
+          arrows: false,
+        },
       },
       {
         breakpoint: 768,
         settings: {
           slidesToShow: 2,
           dots: true,
-          arrows: false
-        }
+          arrows: false,
+        },
       },
       {
         breakpoint: 580,
@@ -71,8 +88,8 @@
           slidesToShow: 1.5,
           slidesToScroll: 1,
           dots: true,
-          arrows: false
-        }
+          arrows: false,
+        },
       },
       {
         breakpoint: 480,
@@ -80,20 +97,25 @@
           slidesToShow: 1.2,
           slidesToScroll: 1,
           dots: true,
-          arrows: false
-        }
+          arrows: false,
+        },
       },
-    ]
+    ],
   });
 
   updateCounter($coachesSlider[0].slick);
 
-  $('.coaches-slider').on(`init reInit`, function (event, slick) {
+  $(".coaches-slider").on(`init reInit`, function (event, slick) {
     updateCounter(slick);
-  })
-  $('.coaches-slider').on(`afterChange`, function (event, slick, currentSlide, nextSlide) {
+  });
+  $(".coaches-slider").on(`afterChange`, function (
+    event,
+    slick,
+    currentSlide,
+    nextSlide
+  ) {
     updateCounter(slick);
-  })
+  });
   $(".slider-btn__arrow.slider-btn__arrow--left").click(function (event) {
     $(".coaches-slider").slick("slickPrev");
   });
@@ -102,7 +124,7 @@
   });
 
   function getSlidesToScroll(slick) {
-    return Math.floor(slick.slickGetOption('slidesToScroll'));
+    return Math.floor(slick.slickGetOption("slidesToScroll"));
   }
 
   function getNumOfSlides(slick) {
@@ -111,13 +133,13 @@
 
   function getCurrSlide(slick) {
     var slidesToScroll = getSlidesToScroll(slick);
-    var currSlide = (slick.slickCurrentSlide() + slidesToScroll) / slidesToScroll;
+    var currSlide =
+      (slick.slickCurrentSlide() + slidesToScroll) / slidesToScroll;
 
     return currSlide;
   }
 
   function updateCounter(slick) {
-    $('.counter').text(getCurrSlide(slick) + ' of ' + getNumOfSlides(slick));
+    $(".counter").text(getCurrSlide(slick) + " of " + getNumOfSlides(slick));
   }
-
 })(jQuery);
