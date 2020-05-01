b61745be744468074899d6ac1f952956cf310c26
FEN <fentisov86@gmail.com>
2020-05-01 18:20:35 +0300

    added fixed header and section banner

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 (function ($, undefined) {
+
+  // Header
+  $(window).on('scroll resize', function () {
+    var header = $('.header'),
+      headerTopPosition = header.offset().top;
+
+    if (headerTopPosition > 0) {
+      header.addClass('header--shadow');
+    } else {
+      header.removeClass('header--shadow');
+    }
+  });
+
+  // Buttons
   $(".burger-menu").on("click", function () {
     $(".main-nav").addClass("active");
     $("body").addClass("lock");
@@ -8,6 +22,7 @@
     $("body").removeClass("lock");
   });
 
+  // First slider
   $(window).on("load resize orientationchange", function () {
     $(".slider-cards").each(function () {
       var $slider = $(this);
@@ -52,6 +67,7 @@
     });
   });
 
+  // Second slider
   var $coachesSlider = $(".coaches-slider").slick({
     arrows: true,
     slidesToShow: 2,
@@ -116,12 +132,6 @@
   ) {
     updateCounter(slick);
   });
-  $(".slider-btn__arrow.slider-btn__arrow--left").click(function (event) {
-    $(".coaches-slider").slick("slickPrev");
-  });
-  $(".slider-btn__arrow.slider-btn__arrow--right").click(function (event) {
-    $(".coaches-slider").slick("slickNext");
-  });
 
   function getSlidesToScroll(slick) {
     return Math.floor(slick.slickGetOption("slidesToScroll"));
@@ -143,6 +153,7 @@
     $(".counter").text(getCurrSlide(slick) + " of " + getNumOfSlides(slick));
   }
 
+  // WOW animation
   wow = new WOW(
     {
       mobile: false
@@ -151,3 +162,170 @@
   wow.init();
 
 })(jQuery);
+
+
+
+// $(function () {
+//   //init WOW.js
+//   var wow = new WOW({
+//     mobile: false
+//   });
+//   wow.init();
+
+//   // Mobile Menu
+//   $('.burger').on('click', function () {
+//     var menu = $('.header__menu');
+
+//     $(this).toggleClass('burger--active');
+//     $(document.body).toggleClass('no-scroll');
+
+//     if (menu.hasClass('header__menu--active')) {
+//       menu.removeClass('header__menu--active')
+//         .addClass('header__menu--hide');
+//     } else {
+//       menu.removeClass('header__menu--hide')
+//         .addClass('header__menu--active');
+//     }
+//   });
+
+//   // Header
+//   $(window).on('scroll resize', function () {
+//     var header = $('.header'),
+//       headerTopPosition = header.offset().top;
+
+//     if (headerTopPosition > 0) {
+//       header.addClass('header--shadow');
+//     } else {
+//       header.removeClass('header--shadow');
+//     }
+//   });
+
+//   // Scrollto
+//   $('[data-scroll]').on('click', function (e) {
+//     e.preventDefault();
+
+//     var targetSection = $(this).data('scroll'),
+//       headerH = $('.header').innerHeight(),
+//       distance = $(targetSection).offset().top - headerH;
+
+//     $('html,body').animate({
+//       scrollTop: distance
+//     }, 1000, 'swing');
+
+//     $('.header__menu').removeClass('header__menu--active');
+//     $('.burger').removeClass('burger--active');
+//   });
+
+//   // ArrowTop
+//   $(window).on('scroll resize', function () {
+//     var arrow = $('.arrow-up'),
+//       bannerH = $('.banner').outerHeight(),
+//       wScroll = $(window).scrollTop();
+
+//     if (wScroll > bannerH) {
+//       arrow.addClass('arrow-up--active');
+//     } else {
+//       arrow.removeClass('arrow-up--active');
+//     }
+//   });
+
+//   // Services Slider
+//   $(window).on('load resize orientationchange', function () {
+//     $('.services__slider').each(function () {
+//       var slider = $(this);
+
+//       if ($(window).width() > 768) {
+//         if (slider.hasClass('slick-initialized')) {
+//           slider.slick('unslick');
+//         }
+//       } else {
+//         if (!slider.hasClass('slick-initialized')) {
+//           slider.slick({
+//             arrows: false,
+//             dots: true,
+//             variableWidth: true,
+//             slideToScroll: 1
+//           });
+//         }
+//       }
+//     });
+//   });
+
+//   // Coaches Slider
+//   var sliderWrap = $('.coaches__slider');
+//   var currentSliderBox = $('.coaches__slider-current');
+//   var totalSliderBox = $('.coaches__slider-total');
+
+//   sliderWrap.on('init afterChange', function (event, slick) {
+//     setCounter(slick);
+//   });
+
+//   sliderWrap.slick({
+//     infinite: false,
+//     initialSlide: 0,
+//     slidesToShow: 2,
+//     slidesToScroll: 2,
+//     appendArrows: '.coaches__slider-counter',
+//     prevArrow: '<a class="arrow-prev"><i class="icon icon-arrow-prev"</a>',
+//     nextArrow: '<a class="arrow-next"><i class="icon icon-arrow-next"</a>',
+//     responsive: [
+//       {
+//         breakpoint: 952,
+//         settings: {
+//           slidesToShow: 1.6,
+//           slidesToScroll: 1
+//         }
+//       },
+//       {
+//         breakpoint: 769,
+//         settings: {
+//           slidesToShow: 2.4,
+//           arrows: false,
+//           dots: true
+//         }
+//       },
+//       {
+//         breakpoint: 620,
+//         settings: {
+//           slidesToShow: 2.2,
+//           dots: true
+//         }
+//       },
+//       {
+//         breakpoint: 520,
+//         settings: {
+//           slidesToShow: 1.6,
+//           slidesToScroll: 1,
+//           dots: true
+//         }
+//       },
+//       {
+//         breakpoint: 400,
+//         settings: {
+//           slidesToShow: 1.3,
+//           slidesToScroll: 1,
+//           dots: true
+//         }
+//       }
+//     ]
+//   });
+
+//   function setCounter(slick) {
+//     var slides = slick.slideCount,
+//       slidesToShow = Math.floor(slick.options.slidesToShow),
+//       slideToScroll = slick.options.slidesToScroll,
+//       currentSlide = (slick.currentSlide + slideToScroll) / slideToScroll;
+
+//     var totalSlidesList;
+
+//     if (slides > slidesToShow) {
+//       totalSlidesList = Math.ceil((slides - slidesToShow) / slideToScroll + 1);
+
+//     } else {
+//       totalSlidesList = currentSlide;
+//     }
+
+//     currentSliderBox.text(currentSlide);
+//     totalSliderBox.text(totalSlidesList);
+//   }
+// });
